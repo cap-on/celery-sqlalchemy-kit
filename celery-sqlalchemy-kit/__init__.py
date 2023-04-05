7e60b6d4e3d52aac6149e50fc52968107b7481cc
“Amelie <amelie.luecke@cap-on.de>
2023-04-05 13:15:37 +0200

    fix __init__.py

--- celery-sqlalchemy-kit/__init__.py
+++ celery-sqlalchemy-kit/__init__.py
@@ -2,9 +2,9 @@
 We import these so later people can import like
 from celery_sqlalchemy_kit import SyncTask
 """
-__version__ = "0.1.1"
 
-from base_task import SyncTask, AsyncTask  # noqa
-from db.model import Routine  # noqa
-from db.session import SessionWrapper  # noqa
-from db.crud import crud  # noqa
+from base_task import SyncTask as SyncTask # noqa
+from base_task import AsyncTask as AsyncTask # noqa
+from db.model import Routine  as Routine# noqa
+from db.session import SessionWrapper as SessionWrapper # noqa
+from db.crud import crud  as crud # noqa
